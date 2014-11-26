c1dcce9407c0657e2ba90a61c1aa32a64199ff72
Platane <brongniart.arthur@gmail.com>
2014-11-26 22:07:41 +0100

    bundle and minify js

--- gulp/tasks/build/html.js
+++ gulp/tasks/build/html.js
@@ -1,10 +1,52 @@
-var   gulp = require('gulp')
-	, htmlmin = require('gulp-html-minifier')
+var gulp = require('gulp')
+  , htmlmin = require('gulp-html-minifier')
+  , Stream = require('promisableStream')
 
+var uid = 0
+
+var importBundle = function(htmlString){
+
+  var lines = htmlString.split(/\n/)
+  var m, i
+
+  for (i=0; i<lines.length; i++)
+    if( (m=lines[i].match(/[\t ]*<!-- *make *bundle *\[ *([^\]]+) *\] *-->/)) )
+      break
+
+  if (i>=lines.length)
+    return htmlString
+
+  var start = i
+  var bundleName = m.concat( './bundle'+(uid++) )[1]
+
+  for (;i<lines.length;i++)
+    if( (m=lines[i].match(/[\t ]*<!-- *end *bundle *-->/)) )
+      break
+
+  lines.splice( start, i-start+1, '<script src="'+bundleName+'"></script>')
+  return lines.join('\n')
+}
+var bundleHTML = function(){
+
+  var stream = new Stream()
+  stream.writable = stream.readable = true
+
+  var pending = 0
+
+  stream.write = function( file ){
+    file.contents = new Buffer( importBundle(file.contents.toString('utf8')) )
+    this.emit('data', file)
+  }
+  stream.end = function(){
+    this.emit('end')
+  }
+
+  return stream
+}
 
 gulp.task('build.html', function() {
   return gulp.src('../sources/**/*.html')
-  	.pipe(htmlmin())
+  	.pipe(bundleHTML())
     .pipe(gulp.dest('../build/'));
 });
 
--- gulp/tasks/gen/works.js
+++ gulp/tasks/gen/works.js
@@ -11,7 +11,7 @@ var parseDate = function( s ){
     return  new Date( parseInt( m[2] ) , parseInt( m[1] )-1 , parseInt( m[0] ) )
 }
 var prepareData = function( data ){
-        
+
     data=JSON.parse( data )
 
     data.works = data.works
@@ -34,14 +34,14 @@ var prepareData = function( data ){
         var weight = Math.sqrt( w['coolness'] * 1.5 * w['weight'] ) *10
         w['rank'] = parseDate( w['to'] || '01/01/2012' ).getTime()
 
-       
+
         w['height'] = Math.max( Math.min( Math.sqrt( (0.4+Math.random()*0.8) * weight )*1.4 , 12 ) , 4 )*45
 
         w['id'] = w['title'].split(' ').join('-')
 
         return w
     })
-    
+
     .sort(function(a,b){
         return a.rank < b.rank ? 1 : -1
     });
@@ -54,8 +54,8 @@ var prepareData = function( data ){
 }
 
 gulp.task('gen.works', function() {
-    
-    templating( '../sources/templates/works.html' , '../build/data/works.json' , prepareData )
+
+    templating( '../build/templates/works.html' , '../build/data/works.json' , prepareData )
     .pipe( htmlmin() )
     .pipe( rename('works.html') )
     .pipe( gulp.dest('../build/') )
