27262df2ef8e86022a4c48cf32fa1436ab791e3a
platane <me@platane.me>
2018-07-16 15:03:55 +0100

    remove CNAME

--- src/content/meta.js
+++ src/content/meta.js
@@ -3,7 +3,7 @@ import image_url from '../asset/image/arthur_brongniart.jpg'
 export default {
   image: image_url,
   title: 'platane homepage',
-  url: 'https://platane.me',
+  url: 'https://platane.github.io',
   description: 'Arthur Brongniart, gifted web developper.',
   locale: 'en',
 }
